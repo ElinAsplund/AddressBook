c817475aa18d694ca23a7c02dde323789acc4a5d
ElinAsplund <elin.asplund91@gmail.com>
2023-01-09 12:18:08 +0100

    Add project files.

---
+++ AddressBook/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AddressBook.Interfaces;
+
+namespace AddressBook.Models
+{
+    internal class Person : IPerson
+    {
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+    }
+}
