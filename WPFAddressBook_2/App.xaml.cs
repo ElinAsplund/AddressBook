6e605ad4c3d1756e7a190cc7d3acce6ba94f3077
Elin Asplund <elin.asplund91@gmail.com>
2023-01-17 18:53:17 +0100

    I made a new project, WPFAddressBook_2 : the rendering is now fixed. Hence I will continue work in this project.

---
+++ WPFAddressBook_2/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using WPFAddressBook_2.MVVM.ViewModels;
+
+namespace WPFAddressBook_2
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            MainWindow = new MainWindow()
+            {
+                DataContext = new MainViewModel()
+            };
+
+            MainWindow.Show();
+            base.OnStartup(e);
+        }
+    }
+}
---
+++ WPFAddressBook_2/MVVM/ViewModels/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFAddressBook_2.MVVM.ViewModels
+{
+    internal partial class MainViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private ObservableObject currentViewModel;
+
+        public MainViewModel()
+        {
+            CurrentViewModel = new ContactsViewModel();
+        }
+    }
+}
